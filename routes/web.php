e60ed1935951a8f1ac881b5e6a44a6dcf646329b
Froilan Najera Morales <froilannajeramorales@gmail.com>
2025-09-19 17:53:43 -0600

    Mis cambios antes de cambiar de rama

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::middleware(['auth'])->group(function () {
     })->middleware('auth')->name('datos-personales');
 
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
